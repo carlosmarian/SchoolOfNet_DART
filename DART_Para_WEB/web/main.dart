755dd27f07ad625d1e7ae1b578c3498017977629
Carlos H. Marian <carlos.marian@amcom.com.br>
2020-01-17 18:36:30 -0300

    Serach elemsnts and choice values

--- DART_Para_WEB/web/main.dart
+++ DART_Para_WEB/web/main.dart
@@ -2,4 +2,18 @@ import 'dart:html';
 
 void main() {
   querySelector('#output').text = 'Curso DART Web.';
+
+  ElementList divs = querySelectorAll('ul li');
+
+  List cursos = [
+    'Curso PHP',
+    'Curso HTML',
+    'Curso Delphi',
+    'Curso C#',
+    'Curso DART',
+    ];
+
+  for (var i = 0 ; i < divs.length; i++){
+    divs[i].text = cursos[i];
+  }
 }
